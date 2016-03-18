fd474009b4f2d2691ee1fe4399c9fd190db6025d
Elsa Filippidou <elsa.filippidou@gmail.com>
2016-03-18 18:48:34 +0100

    connect to API

--- UsersAPI/ViewController.m
+++ UsersAPI/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @end
 
@@ -16,12 +16,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _titleLabel.text = @"Users";
+    
+    [self getData];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) getData {
+    
+    NSURL *url = [NSURL URLWithString:@"http://jsonplaceholder.typicode.com/users"];
+    NSURLSession *session = [NSURLSession sharedSession];
+    
+    // request data, wait until completed, use the data
+    NSURLSessionDataTask *task = [session dataTaskWithURL:url
+                                        completionHandler:^(NSData *data, NSURLResponse *response, NSError *connectionError) {
+                                            NSError *error;
+                                            NSArray *userData = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+                                            
+                                            // save user info in core data
+                                            for (int i=0; i < userData.count; i++) {
+                                                
+                                            }
+                                            
+                                            // update UI in main thread
+                                            dispatch_sync(dispatch_get_main_queue(), ^{
+                                                [_tableView reloadData];
+                                            });
+                                            
+                                        }
+                                  ];
+    [task resume];
+}
+
+# pragma mark - Table View delegate methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1; // change this
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell;
+    
+    // cell.textLabel.text = ...
+    
+    return cell;
 }
 
 @end
